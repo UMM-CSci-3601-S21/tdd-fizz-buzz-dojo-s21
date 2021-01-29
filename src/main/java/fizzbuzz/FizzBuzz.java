9ad91a676b368ef0175ea2db6a723ded7047d631
Nic McPhee <mcphee@morris.umn.edu>
2021-01-28 18:53:35 -0600

    Test zero and five
    This tests zero and five, which both work, and we refacted out the
    duplication of creating the FizzBuzz class.

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -4,4 +4,11 @@ public class FizzBuzz {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+	public String fizzbuzz(int i) {
+        if (i == 5) {
+            return "Buzz";
+        }
+		return "FizzBuzz";
+	}
 }
--- src/test/java/fizzbuzz/FizzBuzzSpec.java
+++ src/test/java/fizzbuzz/FizzBuzzSpec.java
@@ -1,5 +1,27 @@
 package fizzbuzz;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzSpec {
-    
+
+    private FizzBuzz fb;
+
+    @BeforeEach
+    public void setUp() {
+        fb = new FizzBuzz();
+    }
+
+    @Test
+    public void testZero() {
+        assertEquals("FizzBuzz", fb.fizzbuzz(0));
+    }
+
+    @Test
+    public void testFive() {
+        assertEquals("Buzz", fb.fizzbuzz(5));
+    }
+
 }
